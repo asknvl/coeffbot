cc0b69c91d486f458f7152fa91e0945c5203ec38
Alexey Konovalov <asknvl@gmail.com>
2024-02-27 15:31:45 +0300

    wip

--- coeffbot/Models/bot/coeff_bot_v0.cs
+++ coeffbot/Models/bot/coeff_bot_v0.cs
@@ -189,7 +189,7 @@ namespace coeffbot.Models.bot
                         break;
 
                     case "lose":
-                        m = MessageProcessor.GetMessage("lose", url: "https://aviatorprime.space");
+                        m = MessageProcessor.GetMessage("lose", url: "https://aviatorprime.space");//
                         await m.Send(chat, bot);
                         break;
 
